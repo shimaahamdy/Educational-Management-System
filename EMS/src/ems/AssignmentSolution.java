cc5a37105c8d8486e3595d9beafd4e60f13f1e65
shimaa hamdy <shimaahmady722@gmail.com>
2020-10-19 08:54:38 +0200

    comments and small changes in course

--- EMS/src/ems/AssignmentSolution.java
+++ EMS/src/ems/AssignmentSolution.java
@@ -10,46 +10,54 @@ package ems;
  * @author Leader
  */
 public class AssignmentSolution {
-    private ASSIGNMENT assignment; //realtion with assignment
-    private String answer;
-    private int student_grade;
-    private int student_id;   //realtion with student
-    private String comment;
-    private boolean grade_seted;
+    private ASSIGNMENT assignment; //realtion with assignment belong to
+    private final String answer;         // student answer to assignment
+    private int student_grade;     // grade of student of this assignemtn
+    private final int student_id;   //realtion with student that assignment belong
+    private String comment;   // doctor comment to student answers
+    private boolean grade_seted;  // test if grade is set or not
     
-    
+    /* constructor for creation a new solution */
     public AssignmentSolution(String answer,int student_id)
     {
         this.answer=answer;
         this.student_id=student_id;
     }
     
+    /* retuen assignment that answer belong to */
     public ASSIGNMENT get_assignment()
     {
         return this.assignment;
     }
+    /* test if doctor set grade or not */
     public boolean is_grade_set()
     {
         return this.grade_seted;
     }
+    /* set grade of student in assignmnet */
     public void set_student_grade(int student_grade)
     {
         this.student_grade=student_grade;
         this.grade_seted=true;
     }
+    /* set doctor comment to student grade */
     public void set_comment(String comment)
     {
         this.comment=comment;
     }
+    /* return grade of assignemt to student */ 
     public int get_student_grade()
     {
         return this.student_grade;
     }
+    /* return student name of this solution */ 
     public String get_student_name()
     {
         return systemControl.students_list.get(this.student_id).getName();
     }
    
+    
+    /* overview on public solution information */ 
     @Override
     public String toString()
     {
--- EMS/src/ems/COURSE.java
+++ EMS/src/ems/COURSE.java
@@ -12,68 +12,83 @@ import java.util.ArrayList;
  * @author Leader
  */
 public class COURSE {
-    private String name;
-    private String code;
-    private final int number;        
-    ArrayList<STUDENT> student_list;
+    private String name;                   // course name 
+    private String code;                   // course code in colleage
+    private final int number;              // course number in courses list in colleage 
+    ArrayList<STUDENT> student_list;       // students that rolled in course (realtion with students)
     private int  doctor_id;                //realtion with doctor
-    ArrayList<TAS>TAS_list;
-    private int assignment_num;
-    private ASSIGNMENT []assignment_list;   //realtion with assignments
+    ArrayList<TAS>TAS_list;                // which assistant supervise to that course           
+    ArrayList<ASSIGNMENT> assignment_list; // number of assignment has bee upload to this course(realtion with assignments)
     
+    /* constructor in creating new course 
+    course number is created atomiacally by the system 
+    */
     public COURSE(String name,String code)
     {
         this.name=name;
         this.code=code;
-        this.number=systemControl.courses_list.size();
+        this.number=systemControl.courses_list.size();   // automatic generation 
         student_list=new ArrayList<>();
         TAS_list=new ArrayList<>();
     }
-    public ASSIGNMENT [] get_assignment_list()
+    /*return assignments of this course */
+    public ArrayList<ASSIGNMENT> get_assignment_list()
     {
         return this.assignment_list;
     }
+    /* retuen course number */
     public int get_course_number()
     {
         return this.number;
     }
+    /* reurn specific assignment in the course */
     public ASSIGNMENT get_assignment(int assignment_num)
     {
-        return this.assignment_list[assignment_num];
+        return this.assignment_list.get(assignment_num);
     }
-    
+    /* return doctor that responsible of that course */
     public String get_doctor_name()
     {
         return systemControl.doctors_list.get(this.doctor_id).get_name();
     }
     
+    /* add techinical assistant will work with course
+    test if id of tecinical assistant vaild, checking in  TAS list saved in system 
+    if -->true -->add id to tas list belong to this course 
+               -->add course to tas courses that work in         
+    */ 
     public boolean addTAS(int tas)
     {
-        if(tas<systemControl.TAS_list.size() && tas>=systemControl.TAS_list.size())
+        if(tas<systemControl.TAS_list.size() && tas>=systemControl.TAS_list.size()) 
         {
-        this.TAS_list.add(systemControl.TAS_list.get(tas));
-        systemControl.TAS_list.get(tas).add_course(this.number);
+        this.TAS_list.add(systemControl.TAS_list.get(tas)); 
+        systemControl.TAS_list.get(tas).add_course(this.number); 
         return true;
         }
         else return false;
     }
    
+    /* add new assignemnt to the list */
     public void add_assignment(ASSIGNMENT assignment)
     {
-        this.assignment_list[this.assignment_num++]=assignment;
+        this.assignment_list.add(assignment);
     }
+    /* return number of assignments in that course */
     public int get_assignment_num()
     {
-        return this.assignment_num;
+        return this.assignment_list.size();
     }
+    /* add new student that enrolled in course */
     public void add_student(STUDENT student)
     {
         this.student_list.add(student);
     }
+    /* return number of students that enrolled in that course */
     public int get_student_num()
     {
         return this.student_list.size();
     }
+    /* information about course */
     @Override
     public String toString()
     {
